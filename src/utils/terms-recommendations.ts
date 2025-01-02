e30a26cf7fe44f306f1f2225a7511ccd8ca4c92e
refusado <refusado@gmail.com>
2025-01-01 21:13:43 -0300

    recommending related terms based on tags

--- src/utils/terms-recommendations.ts
+++ src/utils/terms-recommendations.ts
@@ -1,7 +1,11 @@
 import { getTermsLocal, Term } from '@/lib/terms';
 
+const RECOMMENDATIONSS_SIZE: number = 12;
+
 export async function getRecommendations(term: Term): Promise<Term[]> {
   const terms = await getTermsLocal();
+
+  const resultIds: string[] = [];
 
   const sameStart: Term[] = terms.filter(({ slug }) => {
     if (slug == term.slug) return false;
@@ -9,11 +13,34 @@ export async function getRecommendations(term: Term): Promise<Term[]> {
     return slug.startsWith(term.slug.split('-')[0]);
   });
 
-  // TODO: get terms with same tag
-  const sameTag: Term[] = [];
+  sameStart.forEach(({ id }) => resultIds.push(id));
 
-  // TODO: get random terms
-  const randomTerms: Term[] = terms.slice(0, 8);
+  const commonTags = terms
+    .filter(({ id }) => id !== term.id && !resultIds.includes(id))
+    .filter(({ tags }) => tags.length > 0)
+    .map((currTerm) => ({
+      term: currTerm,
+      relevance: currTerm.tags.filter((tag) => term.tags.includes(tag)).length,
+    }))
+    .filter(({ relevance }) => relevance > 0)
+    .sort((a, b) => b.relevance - a.relevance)
+    .map(({ term }) => term);
 
-  return [...sameStart, ...sameTag, ...randomTerms].slice(0, 8);
+  const result = [...sameStart, ...commonTags];
+  if (result.length >= RECOMMENDATIONSS_SIZE) {
+    return result
+      .filter(({ id }) => id !== term.id)
+      .slice(0, RECOMMENDATIONSS_SIZE);
+  }
+
+  commonTags.forEach(({ id }) => resultIds.push(id));
+
+  // TODO: make it actually return random terms
+  const randomTerms: Term[] = terms.filter(({ id }) => !resultIds.includes(id));
+
+  const resultWithRandomTerms = [...result, ...randomTerms];
+
+  return resultWithRandomTerms
+    .filter(({ id }) => id !== term.id)
+    .slice(0, RECOMMENDATIONSS_SIZE);
 }
